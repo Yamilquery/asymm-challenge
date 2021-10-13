5d4dd1c5902b9ffafdfc3338131580c0d7990b78
Yamil Díaz Aguirre <yamil@landonearth.com>
2021-10-13 13:22:30 -0500

    docs(add readme and write some docs for the api class)

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# asymm-challenge2
-Created with CodeSandbox
+# Asymm challenge
+
+Asymm interview code challenge
+
+## Run
+
+To execute the challenge, you need to run the following commands.
+
+To install the dependencies, run:
+
+```
+yarn
+```
+
+Configure the environment variables by creating a .env file on the root of your project with the following content:
+
+**.env**
+```
+REACT_APP_API_URL=https://api.spoonacular.com/food/ingredients/
+REACT_APP_API_KEY=YOUR_SPOONACULAR_API_KEY
+REACT_APP_IMAGES_CDN=https://spoonacular.com/cdn/ingredients_500x500
+```
+
+Finally, you can run the project with:
+```
+yarn start
+```
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,34 @@
 import * as axios from 'axios';
 
+/**
+ * Api client
+ * This Api class is connected to axios and provides a client to manage the API requests.
+ *
+ * An environment variable REACT_APP_API_ENDPOINT, needs to be provided in order
+ * to configure correctly.
+ * 
+ * Get all the ingredients example: 
+ * 
+ * ```js
+ * import Api from './api';
+ * const api = new Api();
+ * api.getIngredients({ query: 'banana' }).then(
+ *  (response) => console.log(response.data.results)
+ * );
+ * // This will return a response with the Array of ingredients objects
+ * ```
+ * 
+ * Get information of an ingredient example: 
+ * 
+ * ```js
+ * import Api from './api';
+ * const api = new Api();
+ * api.getIngredients(12345).then(
+ *  (response) => console.log(response.data)
+ * );
+ * // This will return a response with the object of the specified ingredient
+ * ```
+ */
 export default class Api {
   constructor() {
     this.client = null;
