8cc15a061b583b52512f66638e8295b836dcd384
Yamil Díaz Aguirre <yamil@landonearth.com>
2021-10-13 18:25:37 -0500

    fix(fix issue with link)

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,4 +1,4 @@
-import { useHistory, Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 
 const Card = (props) => {
   const history = useHistory();
@@ -13,15 +13,15 @@ const Card = (props) => {
     <div className="w-full py-4 px-4">
       {props.detailed &&  (
         <div className="w-6">
-          <Link onClick={() => history.go(-1)}>
+          <a href="javascript:void(0);" onClick={() => history.go(-1)}>
             <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M11 17l-5-5m0 0l5-5m-5 5h12" />
             </svg>
-          </Link>
+          </a>
         </div>
       )}
 
-      <Link onClick={() => handleClick(props.ingredient.id)}>
+      <a href="javascript:void(0);" onClick={() => handleClick(props.ingredient.id)}>
         <div className="bg-white relative shadow p-2 rounded-lg text-gray-800 hover:shadow-lg">
           {props.detailed &&  (
             <div className="right-0 mt-4 rounded-l-full absolute text-center font-bold text-xs text-white px-2 py-1 bg-green-500">
@@ -43,7 +43,7 @@ const Card = (props) => {
             )}
           </div>
         </div>
-      </Link>
+      </a>
     </div>
   )
 }
