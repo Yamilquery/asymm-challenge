3b86107396640cc396702633867809ebaf10eabc
Yamil Díaz Aguirre <yamil@landonearth.com>
2021-10-13 12:11:02 -0500

    feat(add image & more information to card)

--- src/components/Card.js
+++ src/components/Card.js
@@ -7,6 +7,8 @@ const Card = (props) => {
     if (!props.detailed) history.push(`/${id}`);
   }
 
+  const src_image = `${process.env.REACT_APP_IMAGES_CDN}/${props.ingredient.name}`;
+
   return (
     <div className="w-full py-4 px-4">
       {props.detailed &&  (
@@ -21,16 +23,24 @@ const Card = (props) => {
 
       <a href="javascript:;" onClick={() => handleClick(props.ingredient.id)}>
         <div className="bg-white relative shadow p-2 rounded-lg text-gray-800 hover:shadow-lg">
-          <div className="right-0 mt-4 rounded-l-full absolute text-center font-bold text-xs text-white px-2 py-1 bg-green-500">
-            Ingredient
-          </div>
+          {props.detailed &&  (
+            <div className="right-0 mt-4 rounded-l-full absolute text-center font-bold text-xs text-white px-2 py-1 bg-green-500">
+              Consistency: {props.ingredient.consistency}
+            </div>
+          )}
           
-          <img src={props.ingredient.image} className="h-32 rounded-lg w-full object-cover"/>
+          <img src={src_image} className="h-32 rounded-lg w-full object-cover"/>
           
           <div className="py-2 px-2">
             <div className=" font-bold font-title text-center">
               {props.ingredient.name}
             </div>
+            
+            {props.detailed &&  (
+              <div className="text-gray-700 text-center">
+                {props.ingredient.aisle}
+              </div>
+            )}
           </div>
         </div>
       </a>
