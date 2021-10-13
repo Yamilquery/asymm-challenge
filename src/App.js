2a737897ef0bd4e4588c10a5110c1d82d94f3299
Yamil Díaz Aguirre <yamil@landonearth.com>
2021-10-13 10:58:17 -0500

    feat(add tailwind): add tailwind to the project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./styles.css";
+import "./styles/tailwind-pre-build.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
+      <h1 className="bg-red-300">Asymm Challenge</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
   );
